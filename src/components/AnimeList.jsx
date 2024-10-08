32c1f2cddfbee6190cad4fed002d5a4ac7ea7399
MistaBasco <mistabasco>
2024-10-08 14:10:51 +0100

    reintroduced sidescrolling wheel on animeList

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect, useRef } from "react";
 import AnimeCard from "@/components/AnimeCard";
 import styles from "@/components/AnimeList.module.css";
 
 export default function AnimeList({ animeList }) {
+  const mainContentRef = useRef(null);
+
+  useEffect(() => {
+    const handleWheelScroll = (event) => {
+      if (mainContentRef.current) {
+        // Scroll horizontally instead of vertically
+        mainContentRef.current.scrollLeft += event.deltaY;
+      }
+    };
+
+    const contentDiv = mainContentRef.current;
+    if (contentDiv) {
+      // Add the wheel event listener to the main content
+      contentDiv.addEventListener("wheel", handleWheelScroll);
+    }
+
+    // Cleanup event listener on component unmount
+    return () => {
+      if (contentDiv) {
+        contentDiv.removeEventListener("wheel", handleWheelScroll);
+      }
+    };
+  }, []);
+
   return (
     <>
       <div className={styles.AnimeList}>
